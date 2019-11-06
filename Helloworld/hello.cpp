a5d45e9f0145f04f3ea439fa4d2ac60946a43832
namchokGithub <pech4751@gmail.com>
2019-11-06 10:54:46 +0700

    add C++ first time

---
+++ Helloworld/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    cout << " *** TEST *** \n" ;
+    cout << " --- Hello Namchok ---" ;
+
+    return 0;
+}
