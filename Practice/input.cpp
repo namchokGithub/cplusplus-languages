7eb119fedb559d8b537ab06f9ec1c24ec35dfc6b
namchokGithub <pech4751@gmail.com>
2019-11-18 17:52:41 +0700

    input number

---
+++ Practice/input.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int number;
+    cout << "Please enter number :";
+    cin >> number;
+
+    
+    return 0;
+}
