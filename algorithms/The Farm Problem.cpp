3ed5d95f685ecb071b3b1718c2212264aeb19966
namchokGithub <pech4751@gmail.com>
2019-11-20 17:20:00 +0700

    cal all leg

---
+++ algorithms/The Farm Problem.cpp
@@ -0,0 +1,22 @@
+/*
+ * File name: The Farm Problem
+ * Author: Namechok Singhachai
+ * Date create: 2019-11-20
+ */
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int animals(int chickens, int cows, int pigs) {
+    return (chickens*2) + (chickens*4) + (chickens*4);
+}
+
+int main(int argc, char const *argv[])
+{
+    cout << "\tThe Farm Problem." << endl;
+    cout << "\tHave 2 chickens, 4 cows, 2 pigs" << endl;
+    cout << "\tTotal leg: " << animals(2, 4, 2) << endl;
+
+    return 0;
+}
