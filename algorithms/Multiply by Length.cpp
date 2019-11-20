7fdad0fba95092667643b1279f612dcbeb9b94d6
namchokGithub <pech4751@gmail.com>
2019-11-20 18:58:02 +0700

    multiply By Length

---
+++ algorithms/Multiply by Length.cpp
@@ -0,0 +1,44 @@
+/*
+ * File name: Multiply by Length
+ * Author: Namechok Singhachai
+ * Date create: 2019-11-20
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+vector<int> multiplyByLength(vector<int> arr) {
+    // ------------------------------------------------------ //
+    vector<int> temp;
+    for(int i=0;i<arr.size();i++){
+        int num = arr.at(i) * arr.size();
+        temp.push_back(num);
+    }
+
+	return temp;
+    // ------------------------------------------------------ //
+}
+
+int main(int argc, char const *argv[])
+{
+    vector<int> arr = {2, 3, 1};
+    arr.push_back(0);
+
+    for(int i=0;i<arr.size();i++){
+        cout << arr.at(i) << " " ;
+    }
+
+    cout << "===== After multiply by length" << endl;
+
+    arr = multiplyByLength(arr);
+
+    for(int i=0;i<arr.size();i++){
+        cout << arr.at(i) << " " ;
+    }
+
+    return 0;
+}
