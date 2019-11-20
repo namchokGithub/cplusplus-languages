aa56814750a8f5b355a734b63fb180cf410ce7b2
namchokGithub <pech4751@gmail.com>
2019-11-20 17:55:44 +0700

    char to integer

---
+++ algorithms/Char-to-ASCII.cpp
@@ -0,0 +1,21 @@
+/*
+ * File name: Char-to-ASCII
+ * Author: Namechok Singhachai
+ * Date create: 2019-11-20
+ */
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int ctoa(char c) {
+    // return int(c);
+    return c;
+}
+
+int main(int argc, char const *argv[])
+{
+    cout << "Charactor 'M' : " << ctoa('M') << endl;
+    return 0;
+}
