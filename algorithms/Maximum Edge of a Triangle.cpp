755d89f4ce67a7b41e50d0b78322dfe7dac6b7ba
namchokGithub <pech4751@gmail.com>
2019-11-20 15:50:18 +0700

    add function nextEdge

---
+++ algorithms/Maximum Edge of a Triangle.cpp
@@ -0,0 +1,19 @@
+/*
+ * File name: Maximum Edge of a Triangle.cpp
+ * Author: Namechok Singhachai
+ * Date create: 2019-11-20
+ */
+#include <iostream>
+#include <iomanip>
+
+int nextEdge(int side1, int side2);
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
+
+int nextEdge(int side1, int side2){
+
+}
