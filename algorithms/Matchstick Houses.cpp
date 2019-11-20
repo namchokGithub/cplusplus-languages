b38919f9238542d086bdbac2402574c73fdd6ff0
namchokGithub <pech4751@gmail.com>
2019-11-20 19:22:40 +0700

    edit match house

--- algorithms/Matchstick Houses.cpp
+++ algorithms/Matchstick Houses.cpp
@@ -10,11 +10,16 @@
 using namespace std;
 
 int matchHouses(int step) {
-	return (step*5)+1;
+    // return (step>=1) ? 6+(step-1)*5 : 0;
+    // -------------------------------------------------- //
+	return step!=0 ? (step*5)+1 : 0;
+    // -------------------------------------------------- //
 }
 
 int main(int argc, char const *argv[])
 {
+    cout << "This challenge will help you interpret mathematical relationships both algebraically and geometrically." << endl << endl;
+    cout << "Matchstick Houses 0 : " << matchHouses(0) << endl;
     cout << "Matchstick Houses 1 : " << matchHouses(1) << endl;
     cout << "Matchstick Houses 4 : " << matchHouses(4) << endl;
     cout << "Matchstick Houses 6 : " << matchHouses(6) << endl;
