96af913c6d85b2ba070770ce2c0d73ba999f2fb8
namchokGithub <pech4751@gmail.com>
2020-01-11 15:29:40 +0700

    update

---
+++ algorithms/Return the Highest and Lowest Numbers.cpp
@@ -0,0 +1,22 @@
+/*
+ * File name: Return the Highest and Lowest Numbers
+ * Author: Namechok Singhachai
+ * Date create: 2019-11-5
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+std::string highLow(std::string str) {
+	
+}
+
+int main(int argc, char const *argv[])
+{
+    cout << "There will always be at least one number in the input string." << endl << endl;
+    
+    return 0;
+}
